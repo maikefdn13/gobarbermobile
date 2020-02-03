5a265042615e474a74c40be62167a5e88bc449f9
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-02-03 12:31:26 -0300

    Configurando Root Import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+// import { Container } from './styles';
 
-  render() {
-    return (
-      <View>
-        <Text> App </Text>
-      </View>
-    );
-  }
+export default function App() {
+  return <View />;
 }
