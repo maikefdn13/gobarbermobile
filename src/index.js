dcdc6545648b0d769766d803b573ad765247f26c
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-02-04 10:52:21 -0300

    Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Route from './routes';
 // import { Container } from './styles';
 
 export default function App() {
-  return <Route />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Route />
+    </>
+  );
 }
