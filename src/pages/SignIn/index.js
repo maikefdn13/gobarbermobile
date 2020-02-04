b49ae0ddfadfddfbcf692109a309e768580d4627
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-02-04 10:36:45 -0300

    Pagina SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
-// import { Container } from './styles';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Digite seu e-mail"
+          />
 
-      <Button>Entrar</Button>
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            placeholder="Sua senha secreta"
+          />
+
+          <SubitButton onPress={() => {}}>Acessar</SubitButton>
+
+          <SignLink onPress={() => {}}>
+            <SignLinkText>Criar conta gratuita</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
